f49997ac02b35c7828897d024cc2457a1359527b
YasineOuaga <159425350+YasineOuaga@users.noreply.github.com>
2024-04-30 11:15:46 +0200

    Initial commit

---
+++ PeopleManager.Model/Organization.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeopleManager.Model
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public IList<Person> Members { get; set; } = new List<Person>();
+
+    }
+}
---
+++ PeopleManager.Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeopleManager.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [DisplayName("First name")]
+        [Required]
+        public required string FirstName { get; set; }
+
+        [DisplayName("Last name")]
+        [Required]
+        public required string LastName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        //Optional
+        public int? OrganizationId { get; set; }
+        public Organization? Organization { get; set; }
+    }
+}
